9ce4b3457600a4b853b0ad600b5215a0bf606fb2
hoyahong1101 <hoyahong1101@gmail.com>
2019-11-14 10:16:58 +0900

    2

---
+++ html/20191113_form/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta charset="UTF-8">
+	<title>welcome</title>
+</head>
+<body>
+	<?
+	$name = $_GET["myname"];
+	$email = $_GET["email"];
+	
+	?>	
+
+	이름 : <?echo $name?>
+	email : <?echo $email?>
+	
+</body>
+</html>
